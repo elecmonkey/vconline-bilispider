233d88ea2de2a75dce352f51eca33897d7a705dc
Elecmonkey <lihouzhi2004@hotmail.com>
2020-03-15 14:41:40 +0800

    查询最接近某一时间的数据。

--- server.py
+++ server.py
@@ -1,45 +1,59 @@
 # -- coding: utf-8 --
+from flask import Flask, request, make_response, Response
 from gevent import monkey
-monkey.patch_all()
-
-from flask import Flask
-from gevent import pywsgi
-from flask_restful import reqparse, abort, Api, Resource
 import pymongo
 import config
+import time
+import json
 
+monkey.patch_all()
+app = Flask("vconline-bilispider")
 
-webapi = Flask(__name__)
-api = Api(webapi)
+@app.route('/songs/<aid>', methods=['GET'])
+def songs(aid):
+    myClient = pymongo.MongoClient(config.DATABASE_URL)
+    myDB = myClient[config.DATABASE_INDEX]
+    myCol = myDB["songs"]
+    myQuery = {"aid": int(aid)}
+    myDoc = myCol.find(myQuery)
+    myDoc_r = {}
+    for myDoc_s in myDoc:
+        myDoc_r = myDoc_s
+    if myDoc_r == {}:
+        return { }, 404
+    myDoc_r.pop('_id')
+    res = Response(json.dumps(myDoc_r))
+    res.headers.add("Access-Control-Allow-Origin", "*")
+    # res.set_cookie('a','b',time.time()+360)
+    return res
 
-parser = reqparse.RequestParser()
-parser.add_argument('aid', location=['json', 'args'], type = dict)
+@app.route('/songs/<aid>/data', methods=['GET'])
+def songs_data(aid):
+    myTime = int(request.args.get('time'))
+    myClient = pymongo.MongoClient(config.DATABASE_URL)
+    myDB = myClient[config.DATABASE_DATA]
+    myCol = myDB["av" + str(aid)]
+    myDoc = myCol.find({}) .sort("time", -1)
+    myDoc_r = ''
+    myTimeD = 0
+    i = 0
+    for myDoc_one in myDoc:
+        if i == 0:
+            myTimeD = abs(myTime - myDoc_one['time'])
+            myDoc_r = myDoc_one
+        i += 1
+        myTimeDt = abs(myTime - myDoc_one['time'])
+        if myTimeDt > myTimeD:
+            break
+        myDoc_r = myDoc_one
+        myTimeD = myTimeDt
 
-class video(Resource):
-    def get(self, video_id):
-        myClient = pymongo.MongoClient(config.DATABASE_URL)
-        myDB = myClient[config.DATABASE_INDEX]
-        myCol = myDB["songs"]
-        myQuery = {"aid": int(video_id)}
-        myDoc = myCol.find(myQuery)
-        myDoc_r = {}
-        for myDoc_s in myDoc:
-            myDoc_r = myDoc_s
-        if myDoc_r == {}:
-            abort(404, error="1")
-        myDoc_r.pop('_id')
-        return myDoc_r
-
-    def delete(self, video_id):
-        abort(503, error="1")
-
-    def put(self, video_id):
-        pass
-
-# 监听Url地址
-api.add_resource(video, '/videos/info/<video_id>')
+    myDoc_r.pop("_id")
+    res = Response(json.dumps(myDoc_r))
+    res.headers.add("Access-Control-Allow-Origin", "*")
+    # res.set_cookie('a','b',time.time()+360)
+    return res
 
 if __name__ == '__main__':
-    server = pywsgi.WSGIServer(('0.0.0.0', 3200), webapi)
-    print('RESTfulAPI server start')
-    server.serve_forever()
+    app.run(host='0.0.0.0', port=3200, debug=True)
+

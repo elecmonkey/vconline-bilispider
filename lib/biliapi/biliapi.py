54e64e9cd3abcda42778a49bdc48f21d4a3f2f4e
Elecmonkey <lihouzhi2004@hotmail.com>
2020-03-22 19:35:18 +0800

    新增随机UA

--- lib/biliapi/biliapi.py
+++ lib/biliapi/biliapi.py
@@ -9,10 +9,26 @@ import json
 import urllib.request
 import urllib.parse
 import math
+import random
 
 def getHttpPage(url):
-    Headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE"}
-    return urllib.request.urlopen(urllib.request.Request(url=url, headers=Headers)).read().decode()
+    Headers = [{
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE"    
+    },{
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+    },{
+        "User-Agent":
+        "Mozilla/5.0 (Linux; Android 7.1.1; Mi Note 3 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36"
+    },{
+        "User-Agent":
+        "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"
+    },{
+        "User-Agent":
+        "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+    }]
+    return urllib.request.urlopen(urllib.request.Request(url=url, headers=Headers[random.randint(0,4)])).read().decode()
 
 class Video:
     __aid = 0
